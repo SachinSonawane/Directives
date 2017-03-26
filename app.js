f47e0ad6ef09b0a8f1a5b3b2c42593eab0afc459
Sachin <sachinsonawnae.edu@gamilcom>
2017-03-26 17:57:26 +0530

    sach: isolate scope demo

--- app.js
+++ app.js
@@ -4,5 +4,46 @@
  var app = angular.module('app', []);
 
  app.controller('ctrl', function ($scope) {
+     $scope.callInController = function () {
+         alert("calling in main controller");
+     }
+
      $scope.name = 'sachin';
+     $scope.city= 'sillod';
+ });
+
+ /*
+ 1. templateUrl - we can give path to new html file
+ 2. restrict - Four ways to restrict-
+                a. as element
+                b. as attribute
+                c. as css
+                d. as commment
+ 3. isolate scope: create separate scope from parent controller
+                a. '@' one way binding (from controller to directive), send data in the form of 'STRING'.
+                b. '=' Two way binding, send data as object or string.
+                c. '&' call back function, also we can add multiple events.
+ 4. when scope-
+                a. true - inherits scope properties from parent controller.
+                b. false - using parent controller properties
+                c. {} - separate isolated scope.
+
+ */
+ 
+ app.directive('customDir', function () {
+     return {
+         restrict: 'EAC',
+         templateUrl: 'dir.html',
+         scope: {
+             oneWay: '@',
+             twoWay: '=',
+             callBack: '&'
+         },
+         link: function (scope, iElement, iAttribute) {
+             console.log('calling', scope.oneWay);
+             scope.call = function () {
+                 alert("calling in directive");
+             }
+         }
+     }
  })
